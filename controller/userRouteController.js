72ed5c1875804c0deb6a493dc4bd7860adee238a
sajibur-dev <sajibur.dev@gmail.com>
2022-08-15 22:46:20 +0600

    get user is done]

--- controller/userRouteController.js
+++ controller/userRouteController.js
@@ -1,88 +1,128 @@
-const { hash } = require("../helper/utl")
-const { read, create } = require("../lib/data")
+const { hash, parseJson } = require("../helper/utl");
+const { read, create } = require("../lib/data");
 
+const controller = {};
 
-const controller = {}
+controller.userRouteController = (requestedPropereties, callback) => {
+  const expectedMethod = ["get", "post", "put", "delete"];
+  if (expectedMethod.includes(requestedPropereties.method)) {
+    controller._user[requestedPropereties.method](
+      requestedPropereties,
+      callback
+    );
+  } else {
+    callback(400, {
+      msg: "method not allowed",
+    });
+  }
+};
 
-controller.userRouteController = (requestedPropereties,callback) => {
-    const expectedMethod = ['get','post','put','delete']
-    if(expectedMethod.includes(requestedPropereties.method)){
-        controller._user[requestedPropereties.method](requestedPropereties,callback)
-    } else {
-        callback(400,{
-            msg:'method not allowed'
-        })
-    }
-    
-}
+controller._user = {};
 
-controller._user = {}
+controller._user.post = (requestedPropereties, callback) => {
+  const firstName =
+    typeof requestedPropereties.body.firstName === "string" &&
+    requestedPropereties.body.firstName.trim().length > 0
+      ? requestedPropereties.body.firstName
+      : false;
 
-controller._user.post = (requestedPropereties,callback) => {
-    const firstName = typeof requestedPropereties.body.firstName === 'string' && requestedPropereties.body.firstName.trim().length > 0 ? requestedPropereties.body.firstName : false;
+  const lastName =
+    typeof requestedPropereties.body.lastName === "string" &&
+    requestedPropereties.body.lastName.trim().length > 0
+      ? requestedPropereties.body.lastName
+      : false;
 
-    const lastName = typeof requestedPropereties.body.lastName === 'string' && requestedPropereties.body.lastName.trim().length > 0 ? requestedPropereties.body.lastName : false;
+  const phone =
+    typeof requestedPropereties.body.phone === "string" &&
+    requestedPropereties.body.phone.trim().length === 11
+      ? requestedPropereties.body.phone
+      : false;
 
-    const phone = typeof requestedPropereties.body.phone === 'string' && requestedPropereties.body.phone.trim().length === 11 ? requestedPropereties.body.phone : false;
+  const password =
+    typeof requestedPropereties.body.password === "string" &&
+    requestedPropereties.body.password.trim().length > 0
+      ? requestedPropereties.body.password
+      : false;
 
-    const password = typeof requestedPropereties.body.password === 'string' && requestedPropereties.body.password.trim().length > 0 ? requestedPropereties.body.password : false;
+  const tosAgreement =
+    typeof requestedPropereties.body.tosAgreement === "boolean"
+      ? requestedPropereties.body.tosAgreement
+      : false;
 
-    const tosAgreement = typeof requestedPropereties.body.tosAgreement === 'boolean' ? requestedPropereties.body.tosAgreement : false;
+  const savedUser = {
+    firstName,
+    lastName,
+    phone,
+    password: hash(password),
+    tosAgreement,
+  };
 
-    const savedUser = {
-        firstName,
-        lastName,
-        phone,
-        password:hash(password),
-        tosAgreement 
-    }
+  if (firstName && lastName && phone && password && tosAgreement) {
+    read("users", phone, (err) => {
+      if (err) {
+        create("users", phone, savedUser, (err) => {
+          if (!err) {
+            callback(200, {
+              msg: "data saved is successfull",
+            });
+          } else {
+            callback(500, {
+              error: "server side error",
+            });
+          }
+        });
+      } else {
+        callback(500, {
+          error: "file already exist",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      msg: "bad requeset",
+    });
+  }
+};
 
-    if(firstName && lastName && phone && password && tosAgreement){
-        read('users',phone,(err)=>{
-            if(err){
-                create('users',phone,savedUser,(err)=>{
-                    if(!err){
-                        callback(200,{
-                            msg:'data saved is successfull'
-                        })
-                    } else {
-                        callback(500,{
-                            error:'server side error'
-                        })
-                    }
-                })
-            } else {
-                callback(500,{
-                    error:'file already exist'
-                })
-            }
-        })
-    } else {
-        callback(400,{
-            msg:'bad requeset'
-        })
-    }
-}
+controller._user.get = (requestedPropereties, callback) => {
+  const phone =
+    typeof requestedPropereties.query.phone === "string" &&
+    requestedPropereties.query.phone.trim().length === 11
+      ? requestedPropereties.query.phone
+      : false;
 
+  if (phone) {
+    read("users", phone, (err, data) => {
+      if (!err && data) {
+        const user = parseJson(data);
+        delete user.password;
+        delete user.tosAgreement;
+        callback(200, {
+          data: user,
+        });
+      } else {
+        callback(500, {
+          msg: "user not found",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      msg: "bad request",
+    });
+  }
+};
 
-controller._user.get = (requestedPropereties,callback) => {
-    
-    callback(200,{
-        msg:'get route'
-    })
-}
+controller._user.put = (requestedPropereties, callback) => {
+  callback(200, {
+    msg: "put route",
+  });
+};
 
+controller._user.delete = (requestedPropereties, callback) => {
+  callback(200, {
+    msg: "delete route",
+  });
+};
 
-controller._user.put = (requestedPropereties,callback) => {
-    callback(200,{
-        msg:'put route'
-    })
-}
-
-controller._user.delete = (requestedPropereties,callback) => {
-    callback(200,{
-        msg:'delete route'
-    })
-}
-
-module.exports = controller
+module.exports = controller;

3d8272bf2ef0780b6c9135fa07589707fdaf276b
sajibur-rahman-dev <sajibur.dev@gmail.com>
2022-08-16 14:36:50 +0600

    environments variable edites is done

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const http = require('http')
-const environment = require('./helper/environments')
+const environments = require('./helper/environments')
 const {handleReqRes} = require('./helper/handleReqRes')
 
 const app = {}
 
-app.config = {
-    port : 5000
-}
-
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes)
-    server.listen(environment.port,()=>console.log(`server is running ${environment.port}`))
+    server.listen(environments.port,()=>console.log(`server is running ${environments.port}`))
 }
 
 app.handleReqRes = handleReqRes

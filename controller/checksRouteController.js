c4104270df93a729eac6b1fd45b6fe2b730e2ac1
sajibur-rahman-dev <sajibur.dev@gmail.com>
2022-08-18 00:34:40 +0600

    checks put is done

--- controller/checksRouteController.js
+++ controller/checksRouteController.js
@@ -143,8 +143,7 @@ controller._checks.post = (requestedPropereties, callback) => {
 // get method
 
 controller._checks.get = (requestedPropereties, callback) => {
-    
-    const checksId =
+  const checksId =
     typeof requestedPropereties.query.checksId === "string" &&
     requestedPropereties.query.checksId.trim().length === 20
       ? requestedPropereties.query.checksId
@@ -158,42 +157,42 @@ controller._checks.get = (requestedPropereties, callback) => {
 
   if (checksId) {
     if (tokenId) {
-        read('checks',checksId,(err,data)=>{
-            if(!err && data){
-                const checks = {...parseJson(data)}
-                const checksPhone = checks.phone;
-                _token.verify(tokenId,checksPhone,(isVarified)=>{
-                    if(isVarified){
-                        callback(200,checks)
-                    } else {
-                        callback(403,{
-                            error:'forbidden'
-                        })
-                    }
-                })
+      read("checks", checksId, (err, data) => {
+        if (!err && data) {
+          const checks = { ...parseJson(data) };
+          const checksPhone = checks.phone;
+          _token.verify(tokenId, checksPhone, (isVarified) => {
+            if (isVarified) {
+              callback(200, checks);
             } else {
-                callback(404,{
-                    error:'checks not found'
-                })
+              callback(403, {
+                error: "forbidden",
+              });
             }
-        })
+          });
+        } else {
+          callback(404, {
+            error: "checks not found",
+          });
+        }
+      });
     } else {
-        callback(401,{
-            error:'unauthorize'
-        })
+      callback(401, {
+        error: "unauthorize",
+      });
     }
   } else {
-    callback(405,{
-        error:'bad request'
-    })
+    callback(405, {
+      error: "bad request",
+    });
   }
 };
 
 // put method
 
 controller._checks.put = (requestedPropereties, callback) => {
-    const expectedProtocol = ["http", "https"];
-    const exectedMethod = ["get", "post", "put", "delete"];
+  const expectedProtocol = ["http", "https"];
+  const exectedMethod = ["get", "post", "put", "delete"];
 
   const checksId =
     typeof requestedPropereties.query.checksId === "string" &&
@@ -201,93 +200,88 @@ controller._checks.put = (requestedPropereties, callback) => {
       ? requestedPropereties.query.checksId
       : false;
 
-    
-      const protocol =
-        typeof requestedPropereties.body.protocol === "string" &&
-        expectedProtocol.includes(requestedPropereties.body.protocol)
-          ? requestedPropereties.body.protocol
-          : false;
-    
-      const method =
-        typeof requestedPropereties.body.method === "string" &&
-        exectedMethod.includes(requestedPropereties.body.method)
-          ? requestedPropereties.body.method
-          : false;
-    
-      const url =
-        typeof requestedPropereties.body.url === "string" &&
-        requestedPropereties.body.url.trim().length > 0
-          ? requestedPropereties.body.url
-          : false;
-    
-      const successCode =
-        typeof requestedPropereties.body.successCode === "object" &&
-        requestedPropereties.body.successCode instanceof Array
-          ? requestedPropereties.body.successCode
-          : false;
-    
-      const timeOutSeconds =
-        typeof requestedPropereties.body.timeOutSeconds === "number" &&
-        requestedPropereties.body.timeOutSeconds % 1 === 0 &&
-        requestedPropereties.body.timeOutSeconds >= 1 &&
-        requestedPropereties.body.timeOutSeconds <= 5
-          ? requestedPropereties.body.timeOutSeconds
-          : false;
+  const protocol =
+    typeof requestedPropereties.body.protocol === "string" &&
+    expectedProtocol.includes(requestedPropereties.body.protocol)
+      ? requestedPropereties.body.protocol
+      : false;
 
-          const tokenId =
-          typeof requestedPropereties.headers.tokenid === "string" &&
-          requestedPropereties.headers.tokenid.trim().length === 20
-            ? requestedPropereties.headers.tokenid
-            : false;
+  const method =
+    typeof requestedPropereties.body.method === "string" &&
+    exectedMethod.includes(requestedPropereties.body.method)
+      ? requestedPropereties.body.method
+      : false;
+
+  const url =
+    typeof requestedPropereties.body.url === "string" &&
+    requestedPropereties.body.url.trim().length > 0
+      ? requestedPropereties.body.url
+      : false;
+
+  const successCode =
+    typeof requestedPropereties.body.successCode === "object" &&
+    requestedPropereties.body.successCode instanceof Array
+      ? requestedPropereties.body.successCode
+      : false;
+
+  const timeOutSeconds =
+    typeof requestedPropereties.body.timeOutSeconds === "number" &&
+    requestedPropereties.body.timeOutSeconds % 1 === 0 &&
+    requestedPropereties.body.timeOutSeconds >= 1 &&
+    requestedPropereties.body.timeOutSeconds <= 5
+      ? requestedPropereties.body.timeOutSeconds
+      : false;
+
+  const tokenId =
+    typeof requestedPropereties.headers.tokenid === "string" &&
+    requestedPropereties.headers.tokenid.trim().length === 20
+      ? requestedPropereties.headers.tokenid
+      : false;
 
   if (checksId) {
-
-    
     if (protocol || method || url || successCode || timeOutSeconds) {
-        read('checks',checksId,(err,data)=>{
-            if(!err && data){
-                const checks = {...parseJson(data)}
-                const checksPhone = checks.phone;
-                _token.verify(tokenId, checksPhone, (isVarified) => {
-                    if (isVarified) {
-                        if (protocol) {
-                            checks.protocol = protocol;
-                          }
-                          if (method) {
-                            checks.method = method;
-                          }
-                          if (url) {
-                            checks.url = url;
-                          }
-                          if(successCode){
-                            checks.successCode = successCode
-                          }
-                          if(timeOutSeconds){
-                            checks.timeOutSeconds = timeOutSeconds
-                          }
-            
-                          update("checks", checksId, checks, (err) => {
-                            if (!err) {
-                              callback(200, {
-                                msg: "updated successfull",
-                              });
-                            } else {
-                              callback(500, {
-                                error: "server side error",
-                              });
-                            }
-                          });
-                    } else {
-                      callback(403, {
-                        error: "forbidden",
-                      });
-                    }
-                  });
-            } else {
+      read("checks", checksId, (err, data) => {
+        if (!err && data) {
+          const checks = { ...parseJson(data) };
+          const checksPhone = checks.phone;
+          _token.verify(tokenId, checksPhone, (isVarified) => {
+            if (isVarified) {
+              if (protocol) {
+                checks.protocol = protocol;
+              }
+              if (method) {
+                checks.method = method;
+              }
+              if (url) {
+                checks.url = url;
+              }
+              if (successCode) {
+                checks.successCode = successCode;
+              }
+              if (timeOutSeconds) {
+                checks.timeOutSeconds = timeOutSeconds;
+              }
 
+              update("checks", checksId, checks, (err) => {
+                if (!err) {
+                  callback(200, {
+                    msg: "updated successfull",
+                  });
+                } else {
+                  callback(500, {
+                    error: "server side error",
+                  });
+                }
+              });
+            } else {
+              callback(403, {
+                error: "forbidden",
+              });
             }
-        })
-      
+          });
+        } else {
+        }
+      });
     } else {
       callback(400, {
         msg: "bad request",

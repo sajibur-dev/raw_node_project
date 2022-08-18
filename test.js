2890301f902e9a9d74daab802ce6ca3b3450d23f
sajibur-rahman-dev <sajibur.dev@gmail.com>
2022-08-18 14:06:50 +0600

    notifiacation function is done

--- test.js
+++ test.js
@@ -1,7 +1,2 @@
 const { sendTwilioSms } = require("./helper/notification");
 const { createRandomStr } = require("./helper/utl");
-
-
-sendTwilioSms('01830394432','hello world',(res)=>{
-    console.log('res is',res);
-})
